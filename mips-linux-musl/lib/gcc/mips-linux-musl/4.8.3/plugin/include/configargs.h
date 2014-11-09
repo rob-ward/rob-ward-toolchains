b6016270a28c49c0cda8a53085817199c3781f2d
Rob Ward <robert.ward114@googlemail.com>
2014-11-09 19:20:26 +0000

    Add mips-linux-musl toolchain

---
+++ mips-linux-musl/lib/gcc/mips-linux-musl/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/opt/cross/mips-linux-musl --target=mips-linux-musl --enable-languages=c,c++ --disable-libmudflap --disable-libsanitizer --disable-nls --disable-multilib --with-multilib-list=";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "llsc", "llsc" } };
