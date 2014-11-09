e50c8ed001b7eabb30681a8ec0b14f1e9fd3bf84
Rob Ward <robert.ward114@googlemail.com>
2014-11-09 19:25:52 +0000

    Add arm-linux-musleabi toolchain

---
+++ arm-linux-musleabi/lib/gcc/arm-linux-musleabi/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/opt/cross/arm-linux-musleabi --target=arm-linux-musleabi --enable-languages=c,c++ --disable-libmudflap --disable-libsanitizer --disable-nls --disable-multilib --with-multilib-list= --with-arch=armv7-a --with-float=softfp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv7-a" }, { "float", "softfp" }, { "tls", "gnu" } };
