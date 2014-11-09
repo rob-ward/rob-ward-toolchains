b496d5997d02ea0b4cfe8713c61252afd36699b1
Rob Ward <robert.ward114@googlemail.com>
2014-11-09 19:21:54 +0000

    Add arm-linux-musleabihf toolchain

---
+++ arm-linux-musleabihf/lib/gcc/arm-linux-musleabihf/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/opt/cross/arm-linux-musleabihf --target=arm-linux-musleabihf --enable-languages=c,c++ --disable-libmudflap --disable-libsanitizer --disable-nls --disable-multilib --with-multilib-list= --with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv7-a" }, { "float", "hard" }, { "fpu", "vfpv3-d16" }, { "tls", "gnu" } };
