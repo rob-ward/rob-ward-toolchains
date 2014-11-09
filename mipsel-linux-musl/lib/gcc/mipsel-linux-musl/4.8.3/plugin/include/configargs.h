143ff17f638f79e7e46a89f55692cfc21d485155
Rob Ward <robert.ward114@googlemail.com>
2014-11-09 19:20:58 +0000

    Add mipsel-linux-musl toolchain

---
+++ mipsel-linux-musl/lib/gcc/mipsel-linux-musl/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix=/opt/cross/mipsel-linux-musl --target=mipsel-linux-musl --enable-languages=c,c++ --disable-libmudflap --disable-libsanitizer --disable-nls --disable-multilib --with-multilib-list=";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "llsc", "llsc" } };
